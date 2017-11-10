df46991fd1c3d96461313fe3d6d52902fc5165a5
Kasper Daugaard <kasdaug@gmail.com>
2017-11-10 11:52:34 +0100

    New facebook implementation that fcking works

--- andersen/app/src/main/java/nordahl/andersen/MainActivity.java
+++ andersen/app/src/main/java/nordahl/andersen/MainActivity.java
@@ -2,37 +2,40 @@ package nordahl.andersen;
 
 //https://github.com/firebase/FirebaseUI-Android/tree/master/auth#configuration
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         FirebaseAuth auth = FirebaseAuth.getInstance();
-        if (auth.getCurrentUser() != null) {
-            // already signed in
-        } else {
-           //toast?? "not logged in"
-        }
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+     //   startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(Arrays.asList(AuthUI.FACEBOOK_PROVIDER)).build(),1);
 
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(
                                 Arrays.asList(
-                                        new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
-                        .build(),
-                RC_SIGN_IN);
+                                        new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()
+                                        ))
+                        .build(),1);
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
     }
+
 }
